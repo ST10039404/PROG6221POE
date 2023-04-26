a0ed235b9ead135c7a9563d0e5c74a10e7747dae
ST10039404 <ST10039404@vcconnect.edu.za>
2023-04-26 10:31:34 +0200

    added comments to Program class

--- PROG6221POE/Program.cs
+++ PROG6221POE/Program.cs
@@ -65,30 +65,54 @@ public class ConsoleRecipe
 
 
     ////  Function Methods  ////////////////////////////////////////////////////////////////////////////////////////////////
+    //gets values necessary for Recipe constructor which would include the recipe name, number of ingredients and number of steps.
     public Recipe GenerateNewRecipe()
     {
         string recipeName = " ";
         int numIngredients = 0;
         int numSteps = 0;
+        string continueVal;
 
+        //all 3 follow the same format, checks for a continue value (asks if user would like to lock in their choice after displaying value back to them) then has a check for if value would be illegal or mess with array (empty or null)
+        //except for method 2 which checks if value is less than 0 (you cant have a ingredient with 0 or less quantity)
         Console.WriteLine("///////////////////");
+        continueVal = "n";
+        //checks for user validation that they would like to lock in input.
+        while (continueVal.ToUpper().Equals("y") == false)
+        {
+            //checks for invalid entry to illogical entry.
             while (String.IsNullOrWhiteSpace(recipeName))
             {
                 Console.Write("\nInput the name of your recipe >>>  ");
                 recipeName = Console.ReadLine();
             }
+            Console.Write("Is this the name you would like to save?:\n {0} \n (Y) to save entry. Anything else to re-enter.\n  >> ");
+            continueVal = Console.ReadLine();
+        }
 
+        continueVal = "n";
+        while (continueVal.ToUpper().Equals("y") == false)
+        {
             while (numIngredients <= 0)
             {
                 Console.Write("\n\nInput the number of ingredients >>>  ");
                 numIngredients = Convert.ToInt32(Console.ReadLine());
             }
+            Console.Write("Is this the quantity you would like to save?:\n {0} \n (Y) to save entry. Anything else to re-enter.\n  >> ");
+            continueVal = Console.ReadLine();
+        }
 
-        while (numSteps <= 0)
+        continueVal = "n";
+        while (continueVal.ToUpper().Equals("Y") == false)
+        {
+            while (numSteps <= 0)
             {
                 Console.Write("\n\nInput the number of steps for your recipe >>>  ");
                 numSteps = Convert.ToInt32(Console.ReadLine());
             }
+            Console.Write("Is this the measurement you would like to save?:\n {0} \n (Y) to save entry. Anything else to re-enter.\n  >> ");
+            continueVal = Console.ReadLine();
+        }
         Console.WriteLine("///////////////////");
 
         Recipe returnedRecipe = new Recipe(recipeName, numIngredients, numSteps);
@@ -98,12 +122,15 @@ public class ConsoleRecipe
 
     public void DisplayRecipe(Recipe yourRecipe)
     {
+        //checks for empty recipe so it can exit if it is.
         if (yourRecipe == null)
         {
             Console.WriteLine("Error Occured: Recipe is null!");
+            return;
         }
         else 
         {
+            //checks for empty values so it can exit if it is.
             if (yourRecipe.getIngredientsArray() == null || yourRecipe.getStepsArray() == null)
             {
                 Console.WriteLine("Error occured: Recipe contains nulls");
@@ -133,14 +160,18 @@ public class ConsoleRecipe
     }
 
     
+    //uses the 4th index in every ingredient entry (which when displayed will have an effect on the quantity which shows up) so it would be: quantity * index4 = displayQuantity.
     public void MultiplyFactorial(Recipe yourRecipe)
     {
+        //checks for empty recipe so it can exit if it is.
         if (yourRecipe == null)
         {
             Console.WriteLine("Error Occured: Recipe is null!");
+            return;
         }
         else
         {
+            //checks for empty values so it can exit if it is.
             if (yourRecipe.getIngredientsArray() == null)
             {
                 Console.WriteLine("Error occured: Recipe contains nulls");
@@ -182,9 +213,11 @@ public class ConsoleRecipe
         }
     }
 
+    //turns yourRecipe into a new Recipe which has no entries and no size.
     public void ClearRecipe()
     {
         this.yourRecipe = new Recipe();
+        Console.WriteLine("Recipe is now empty.");
     }
     ////  Function Methods  ////////////////////////////////////////////////////////////////////////////////////////////////
 }
